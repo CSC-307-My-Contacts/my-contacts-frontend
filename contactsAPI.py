4e373d42acbb091c7163658c982f20e152ddcb3f
Yogi Sunkara <sunkara.yogitha@gmail.com>
2020-05-10 14:25:33 -0700

    wrote a update method

--- contactsAPI.py
+++ contactsAPI.py
@@ -20,30 +20,10 @@ CORS(app)  # Here we'll allow requests coming from any domain. Not recommended f
 #             'name' : 'Charlie',
 #             'job': 'Janitor',
 #         },
-#         # {
-#         #     'id' : 'abc123',
-#         #     'name': 'Mac',
-#         #     'job': 'Bouncer',
-#         # },
-#         # {
-#         #     'id' : 'ppp222',
-#         #     'name': 'Mac',
-#         #     'job': 'Professor',
-#         # },
-#         # {
-#         #     'id' : 'yat999',
-#         #     'name': 'Dee',
-#         #     'job': 'Aspring actress',
-#         # },
-#         # {
-#         #      'id' : 'zap555',
-#         #     'name': 'Dennis',
-#         #     'job': 'Bartender',
-#         # }
 #     ]
 # }
 
-@app.route('/users/<id>/contacts/<id>', methods=['POST', 'DELETE'])
+@app.route('/users/<id>/contacts/<id>', methods=['POST', 'DELETE','PATCH'])
 def get_contact(userId, contactId):
     if request.method == 'DELETE':
         user = User({"_id": userId})
@@ -53,6 +33,14 @@ def get_contact(userId, contactId):
         if resp["n"] == 1:
             return jsonify(success=True), 200
         return {}, 204
+    if request.method == 'PATCH':
+        contactToUpdate = request.get_json()
+        contactToUpdate = Contacts(contactToUpdate)
+        resp = contactToUpdate.save()
+        if resp["n"] == 1:
+            return jsonify(success=True), 200
+        return {}, 204
+    
 
 def add_contact(userId):
     if request.method == 'POST':
@@ -61,19 +49,11 @@ def add_contact(userId):
         newContact = Contacts(contactToAdd)
         resp = newContact.save()
         user['contact_list'].append(str(newContact['_id']))
-        if resp["n"] == 1 :
+        if resp["n"] == 1:
             return jsonify(success=True), 200
         return {}, 204
-        resp = jsonify(newUser), 201
-        return resp
 
 
-# def find_users_by_name(name):
-#     subdict = {'users_list' : []}
-#     for user in users['users_list']:
-#         if user['name'] == name:
-#             subdict['users_list'].append(user)
-#     return subdict
 
 def find_users_by_name_job(name, job):
     subdict = {'users_list': []}
@@ -81,7 +61,3 @@ def find_users_by_name_job(name, job):
         if user['name'] == name and user['job'] == job:
             subdict['users_list'].append(user)
     return subdict
-
-# def generate_id():
-#     lettersAndDigits = string.ascii_letters + string.digits
-#     return ''.join((random.choice(lettersAndDigits) for i in range(6)))
