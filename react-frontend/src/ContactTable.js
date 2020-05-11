658690ea756058756d95821cadcb0fde3f505d07
Alex MacLean <alex.maclean2000@gmail.com>
2020-05-11 10:07:14 -0700

    super minimal contact list

---
+++ react-frontend/src/ContactTable.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class ContactTable extends Component {
+    state = {
+        contact_list: [],
+    }
+
+    render() {
+        const { contact_list } = this.state
+
+        const rows = this.state.contact_list.map((row, index) => {
+            return (
+                <tr key={index}>
+                    <td>{row.name}</td>
+                    <td>{row.phone}</td>
+                    <td>{row.email}</td>
+                </tr>
+            )
+        })
+
+        return (
+            <div className="container">
+                <tbody>{rows}</tbody>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/10/contacts')
+            .then(res => {
+                const contacts = res.data.contact_list;
+                this.setState({ contact_list: contacts });
+            })
+            .catch(function (error) {
+                //Not handling the error. Just logging into the console.
+                console.log(error);
+            });
+    }
+
+}
+
+export default ContactTable
---
+++ react-frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ContactTable from './ContactTable'
+import './index.css'
+
+ReactDOM.render(<ContactTable />, document.getElementById('root'))
