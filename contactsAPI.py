4a907270f6af6e1ba5802b06e16eec97a4402a22
Yogi Sunkara <sunkara.yogitha@gmail.com>
2020-05-11 21:04:02 -0700

    removed errors from contacts

--- contactsAPI.py
+++ contactsAPI.py
@@ -4,26 +4,14 @@ from flask import jsonify
 from flask_cors import CORS
 import random
 import string
-from db import User, Contacts
+from database import User, Contacts
 
 app = Flask(__name__)
 
 # CORS stands for Cross Origin Requests.
 CORS(app)  # Here we'll allow requests coming from any domain. Not recommended for production environment.
 
-
-# users = {
-#     'users_list' :
-#     [
-#         {
-#             'id' : 'xyz789',
-#             'name' : 'Charlie',
-#             'job': 'Janitor',
-#         },
-#     ]
-# }
-
-@app.route('/users/<id>/contacts/<id>', methods=['POST', 'DELETE','PATCH'])
+@app.route('/users/<userId>/contacts/<contactId>', methods=['POST', 'DELETE','PATCH'])
 def get_contact(userId, contactId):
     if request.method == 'DELETE':
         user = User({"_id": userId})
@@ -40,7 +28,6 @@ def get_contact(userId, contactId):
         if resp["n"] == 1:
             return jsonify(success=True), 200
         return {}, 204
-    
 
 def add_contact(userId):
     if request.method == 'POST':
@@ -52,12 +39,3 @@ def add_contact(userId):
         if resp["n"] == 1:
             return jsonify(success=True), 200
         return {}, 204
-
-
-
-def find_users_by_name_job(name, job):
-    subdict = {'users_list': []}
-    for user in subdict['users_list']:
-        if user['name'] == name and user['job'] == job:
-            subdict['users_list'].append(user)
-    return subdict
