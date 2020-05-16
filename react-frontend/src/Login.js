472dd4d4f3b259c5ec5e8379c247379b5cdd2808
Alex MacLean <alex.maclean2000@gmail.com>
2020-05-16 13:31:45 -0700

    css cleanup

--- react-frontend/src/Login.js
+++ react-frontend/src/Login.js
@@ -16,30 +16,32 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="form-signin text-center">
-        <h1 className="h3 mb-3 font-weight-normal">Please log in</h1>
-        <input
-          type="name"
-          id="uid"
-          name="uid"
-          className="form-control mb-4"
-          placeholder="User ID"
-          required=""
-          onChange={this.handleChange}
-        />
-        <button
-          className="btn btn-lg btn-primary btn-block"
-          onClick={() => {
-            this.props.authenticate(this.state.uid, () =>
-              this.props.history.push("/")
-            );
-          }}
-        >
-          Login
-        </button>
-        <p className="text-muted mt-3">
-          Don't have an account? <Link to="/register">Register</Link>
-        </p>
+      <div className="center-contents h-100">
+        <div className="form-signin text-center">
+          <h1 className="h3 mb-3 font-weight-normal">Please log in</h1>
+          <input
+            type="name"
+            id="uid"
+            name="uid"
+            className="form-control mb-4"
+            placeholder="User ID"
+            required=""
+            onChange={this.handleChange}
+          />
+          <button
+            className="btn btn-lg btn-primary btn-block"
+            onClick={() => {
+              this.props.authenticate(this.state.uid, () =>
+                this.props.history.push("/")
+              );
+            }}
+          >
+            Login
+          </button>
+          <p className="text-muted mt-3">
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+        </div>
       </div>
     );
   }
