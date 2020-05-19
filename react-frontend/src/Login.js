5419ad25e06bbe79dc0189ca9dd8772a5c84dd48
Alex MacLean <alex.maclean2000@gmail.com>
2020-05-18 21:46:57 -0700

    better auth UX

--- react-frontend/src/Login.js
+++ react-frontend/src/Login.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Image from "react-bootstrap/Image";
 
 class Login extends Component {
   state = {
     username: "",
     password: "",
+    valid: true,
   };
 
   handleChange = (event) => {
@@ -21,20 +23,30 @@ class Login extends Component {
     return (
       <div className="center-contents h-100">
         <div className="form-signin text-center">
+          <Image src="/logo192.png" className="mb-4 signin-img" rounded />
           <h1 className="h3 mb-3 font-weight-normal">Please log in</h1>
           <div className="mb-3">
             <Form.Control
               type="text"
               name="username"
               placeholder="Username"
+              className="input-group-top"
               onChange={this.handleChange}
+              value={this.state.username}
+              isInvalid={!this.state.valid}
             />
             <Form.Control
               type="password"
               name="password"
               placeholder="Password"
+              className="input-group-bottom"
               onChange={this.handleChange}
+              value={this.state.password}
+              isInvalid={!this.state.valid}
             />
+            <Form.Control.Feedback type="invalid">
+              Login Failed, Username or Password Invalid
+            </Form.Control.Feedback>
           </div>
           <Button
             variant="primary"
@@ -46,6 +58,7 @@ class Login extends Component {
                 this.state.password,
                 (success) => {
                   if (success) this.props.history.push("/");
+                  else this.setState({ valid: false, password: "" });
                 }
               );
             }}
--- react-frontend/src/Register.js
+++ react-frontend/src/Register.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
 
 class Register extends Component {
   state = {
     username: "",
-    password: "",
+    password1: "",
+    password2: "",
+    match: true,
+    usernameOk: true,
   };
 
   handleChange = (event) => {
@@ -21,31 +25,66 @@ class Register extends Component {
     return (
       <div className="center-contents h-100">
         <div className="form-signin text-center">
+          <Image src="/logo192.png" className="mb-4 signin-img" rounded />
           <h1 className="h3 mb-3 font-weight-normal">Register new account</h1>
-          <Form.Control
-            type="text"
-            name="username"
-            placeholder="Username"
-            onChange={this.handleChange}
-          />
-          <Form.Control
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={this.handleChange}
-          />
+          <Form.Group>
+            <Form.Control
+              type="text"
+              name="username"
+              placeholder="Username"
+              autoComplete="off"
+              value={this.state.username}
+              onChange={this.handleChange}
+              isInvalid={!this.state.usernameOk}
+            />
+            <Form.Control.Feedback type="invalid">
+              Request failed, username already in use
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Control
+              type="password"
+              name="password1"
+              className="input-group-top"
+              placeholder="Password"
+              autoComplete="off"
+              value={this.state.password1}
+              onChange={this.handleChange}
+              isInvalid={!this.state.match}
+            />
+            <Form.Control
+              type="password"
+              name="password2"
+              className="input-group-bottom"
+              placeholder="Confirm Password"
+              autoComplete="off"
+              value={this.state.password2}
+              onChange={this.handleChange}
+              isInvalid={!this.state.match}
+            />
+            <Form.Control.Feedback type="invalid">
+              Passwords must match
+            </Form.Control.Feedback>
+          </Form.Group>
           <Button
             variant="primary"
             size="lg"
             block
             onClick={() => {
-              this.props.registerUser(
-                this.state.username,
-                this.state.password,
-                (success) => {
-                  if (success) this.props.history.push("/");
-                }
-              );
+              if (this.state.password1 === this.state.password2) {
+                this.setState({ match: true });
+                this.props.registerUser(
+                  this.state.username,
+                  this.state.password1,
+                  (success) => {
+                    if (success) this.props.history.push("/");
+                    else this.setState({ usernameOk: false });
+                  }
+                );
+              } else {
+                this.setState({ match: false, usernameOk: true });
+              }
             }}
           >
             Register
