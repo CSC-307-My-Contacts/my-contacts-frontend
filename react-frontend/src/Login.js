e3ecb76b29d170f062bad9be79099fc30111a8d5
Alex MacLean <alex.maclean2000@gmail.com>
2020-05-26 21:00:04 -0700

    waiting is better

--- react-frontend/src/Login.js
+++ react-frontend/src/Login.js
@@ -3,12 +3,14 @@ import { Link, withRouter } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Image from "react-bootstrap/Image";
+import Spinner from "react-bootstrap/Spinner";
 
 class Login extends Component {
   state = {
     username: "",
     password: "",
     valid: true,
+    waiting: false,
   };
 
   handleChange = (event) => {
@@ -19,7 +21,40 @@ class Login extends Component {
     });
   };
 
+  login = () => {
+    this.setState({ waiting: true });
+    this.props.login(this.state.username, this.state.password, () => {
+      this.setState({ valid: false, waiting: false, password: "" });
+    });
+  };
+
   render() {
+    const LoginButton = () => (
+      <Button
+        disabled={this.state.waiting}
+        variant="primary"
+        size="lg"
+        block
+        onClick={this.login}
+      >
+        {this.state.waiting ? (
+          <>
+            <Spinner
+              as="span"
+              animation="border"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+              className="m-1"
+            />{" "}
+            Logging In&hellip;
+          </>
+        ) : (
+          <>Login</>
+        )}
+      </Button>
+    );
+
     return (
       <div className="center-contents h-100">
         <div className="form-signin text-center">
@@ -48,23 +83,7 @@ class Login extends Component {
               Login Failed, Username or Password Invalid
             </Form.Control.Feedback>
           </div>
-          <Button
-            variant="primary"
-            size="lg"
-            block
-            onClick={() => {
-              this.props.login(
-                this.state.username,
-                this.state.password,
-                (success) => {
-                  if (success) this.props.history.push("/");
-                  else this.setState({ valid: false, password: "" });
-                }
-              );
-            }}
-          >
-            Login
-          </Button>
+          <LoginButton />
           <p className="text-muted mt-3">
             Don't have an account? <Link to="/register">Register</Link>
           </p>
